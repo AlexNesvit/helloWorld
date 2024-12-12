43cf74a5f63ecb08763256b0625c43c2198390fb
AlexNesvit <alex.nesvit2000@gmail.com>
2024-12-12 11:11:07 +0100

    node ok

---
+++ main.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello Wilders!"));
+console.log(chalk.red("Hello Wilders!"));
+console.log(chalk.green("Hello Wilders!"));
+console.log(chalk.yellow("Hello Wilders!"));
+
+
